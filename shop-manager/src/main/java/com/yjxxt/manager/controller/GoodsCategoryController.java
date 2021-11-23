b8f153c233e5565b3036af6b42f946b67bcc72f1
your_username <your_email@domain.com>
2021-11-23 14:56:05 +0800

    添加工具类和前端商品界面

--- shop-manager/src/main/java/com/yjxxt/manager/controller/GoodsCategoryController.java
+++ shop-manager/src/main/java/com/yjxxt/manager/controller/GoodsCategoryController.java
@@ -3,28 +3,46 @@ package com.yjxxt.manager.controller;
 import com.yjxxt.manager.pojo.GoodsCategory;
 import com.yjxxt.manager.service.GoodsCategoryService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping("goods_category")
 public class GoodsCategoryController {
     @Resource
     private GoodsCategoryService goodsCategoryService;
-    @GetMapping("/{id}")
-    @ResponseBody
-    public GoodsCategory queryGoodsCategoryById(@PathVariable Short id){
-        return goodsCategoryService.queryGoodsCategoryById(id);
+
+    /**
+     * 商品分类列表
+     *
+     * @return
+     */
+    @RequestMapping("index")
+    public String index(){
+        return "goods/goodsCategory/goods_category_list";
     }
-    @GetMapping("/example/{id}")
-    @ResponseBody
-    public GoodsCategory queryGoodsCategoryByGoodsCategoryExample(@PathVariable Short id){
-        return goodsCategoryService.queryGoodsCategoryByGoodsCategoryExample(id);
+
+    /**
+     * 添加商品
+     * @return
+     */
+    @RequestMapping("addGoodsCategoryPage")
+//    @ResponseBody
+    public String toAddGoodsCategoryPage(Model model){
+        //将查询结果存到model中变量为gclist与前端对应
+        model.addAttribute("gcList",goodsCategoryService.queryGoodsCategoriesByParentId((short) 0));
+        return "goods/goodsCategory/goods_category_add";
     }
+    @RequestMapping("queryGoodsCategoriesByParentId")
+    @ResponseBody
+    public List<GoodsCategory> queryGoodsCategoriesByParentId(Short parentId){
+        return goodsCategoryService.queryGoodsCategoriesByParentId(parentId);
+    }
+
 
 }
 
--- shop-manager/src/main/java/com/yjxxt/manager/service/GoodsCategoryService.java
+++ shop-manager/src/main/java/com/yjxxt/manager/service/GoodsCategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class GoodsCategoryService {
@@ -20,5 +21,16 @@ public class GoodsCategoryService {
         GoodsCategoryExample example=new GoodsCategoryExample();
         example.createCriteria().andIdEqualTo(id);
         return goodsCategoryMapper.selectByExample(example).get(0);
+    }
+
+    /**
+     * 根据上级分类id 查询子分类记录
+     * @param parentId
+     * @return
+     */
+    public List<GoodsCategory> queryGoodsCategoriesByParentId(Short parentId){
+        GoodsCategoryExample example=new GoodsCategoryExample();
+        example.createCriteria().andParentIdEqualTo(parentId);
+       return goodsCategoryMapper.selectByExample(example);
     }
 }
